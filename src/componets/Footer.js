0c4d546a42fb4954f84a15b85550a77bd1f0bf15
vadzim.velikanets <velikanets@mail.ru>
2021-03-03 18:27:09 +0300

    fix: footer

--- src/componets/Footer.js
+++ src/componets/Footer.js
@@ -2,8 +2,11 @@ import React from 'react';
 const Footer = () =>{
     return(
         <footer>
+            <a href="https://rs.school/js/">
+                <img src="./img/rs_school_js.svg" height="30" alt=""/>
+            </a>
             <span>
-               Created by Vadim Velikanets,  2021
+               Created by <a href="https://github.com/VadimVelikanets">Vadim Velikanets</a>,  2021
             </span>
         </footer>
     )
